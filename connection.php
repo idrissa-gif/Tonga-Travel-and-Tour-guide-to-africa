dcc75fcc4869267dc574f36e0121f36335605299
idrissa-gif <idrissa@iut-dhaka.edu>
2022-08-30 10:05:22 +0600

    connection error

--- connection.php
+++ connection.php
@@ -3,8 +3,22 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass ="";
 $dbname = "Tonga_travel_db";
-if(!$con=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-{
-    die("failed to connect!");
+
+/* You should enable error reporting for mysqli before attempting to make a connection */
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// $mysqli = new mysqli('localhost', 'my_user', 'my_password', 'my_db');
+$mysqli = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+/* Set the desired charset after establishing a connection */
+$mysqli->set_charset('utf8mb4');
+
+printf("Success... %s\n", $mysqli->host_info);
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
 }
-?>
+
+
